27fc143f2fb518cd5ccef797d04e16dc17f6fa58
emilykamiti <emilykamiti@gmail.com>
2025-04-03 16:04:23 +0300

    login router

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -22,3 +22,25 @@ router.post("/signup", async (req, res) => {
     res.status(500).json({ error: err.message });
   }
 });
+
+//login
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) return res.status(400).json({ msg: "Invalid credentials" });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).json({ msg: "Invalid credentials" });
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expressIn: "1h",
+    });
+    res.json({ token });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+model.exports = router;
